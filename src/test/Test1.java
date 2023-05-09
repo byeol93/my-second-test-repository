1d9ef7d228267016ab7bc02b377d101d30d8a301
byeol93 <byuuuuul@gmail.com>
2023-05-09 15:01:40 +0900

    집에서 작업

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -3,5 +3,7 @@ package test;
 public class Test1 {
 	public static void main(String[] ar) {
 		System.out.println("프로그램 시작");
+		System.out.println("프로그램 끝");
+		System.out.println("집에서 작업함.");
 	}
 }
